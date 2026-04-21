4564f114cf62a161609e4c90a57cd0ab0c0effb0
Curt Bercht Neto <bercht@live.com>
2026-04-21 00:23:38 -0300

    feat(playbook-suggestions): add PlaybookSuggestionsController

---
+++ app/controllers/playbook_suggestions_controller.rb
@@ -0,0 +1,48 @@
+class PlaybookSuggestionsController < ApplicationController
+  before_action :set_playbook
+  before_action :set_suggestion, only: [:update]
+
+  def create
+    result = Playbooks::GenerateSuggestionsService.call(
+      playbook: @playbook,
+      content_type: params[:content_type],
+      quantity: params[:quantity].to_i
+    )
+
+    if result.success?
+      @suggestions = result.data[:suggestions]
+      respond_to do |format|
+        format.turbo_stream
+      end
+    else
+      @error = result.error
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            "playbook_suggestions_error",
+            partial: "playbook_suggestions/error",
+            locals: { error: @error }
+          )
+        end
+      end
+    end
+  end
+
+  def update
+    authorize @suggestion
+    @suggestion.update!(status: params[:status])
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def set_playbook
+    @playbook = current_tenant.playbooks.find(params[:playbook_id])
+  end
+
+  def set_suggestion
+    @suggestion = current_tenant.playbook_suggestions.find(params[:id])
+  end
+end
